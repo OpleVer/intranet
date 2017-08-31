edd1e538d30ef08f49e72660a988cb4d278f90a7
dayao <dayao.core@gmail.com>
2017-08-31 12:29:15 -0500

    directorio

--- intranet/resources/views/directorio.blade.php
+++ intranet/resources/views/directorio.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Directorio</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -15,7 +15,54 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div class="profile-header row">
+                               <div class="col-lg-2 col-md-4 col-sm-12 text-center">
+                                   <img src="assets/img/avatars/divyia.jpg" alt="" class="header-avatar">
+                               </div>
+                               <div class="col-lg-5 col-md-8 col-sm-12 profile-info">
+                                   <div class="header-fullname">Kim Ryder</div>
+                                   <a href="#" class="btn btn-palegreen btn-sm  btn-follow">
+                                       <i class="fa fa-check"></i>
+                                       Following
+                                   </a>
+                                   <div class="header-information">
+                                       Kim is a software developer in Microsoft. She works in ASP.NET MVC Team and collaborates with other teams.
+                                   </div>
+                               </div>
+                               <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12 profile-stats">
+                                   <div class="row">
+                                       <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 stats-col">
+                                           <div class="stats-value pink">284</div>
+                                           <div class="stats-title">FOLLOWING</div>
+                                       </div>
+                                       <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 stats-col">
+                                           <div class="stats-value pink">803</div>
+                                           <div class="stats-title">FOLLOWERS</div>
+                                       </div>
+                                       <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 stats-col">
+                                           <div class="stats-value pink">1207</div>
+                                           <div class="stats-title">POSTS</div>
+                                       </div>
+                                   </div>
+                                   <div class="row">
+                                       <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 inlinestats-col">
+                                           <i class="glyphicon glyphicon-map-marker"></i> Boston
+                                       </div>
+                                       <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 inlinestats-col">
+                                           Rate: <strong>$250</strong>
+                                       </div>
+                                       <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 inlinestats-col">
+                                           Age: <strong>24</strong>
+                                       </div>
+                                   </div>
+                               </div>
+                           </div>
+
+
+
+
+
+
                 </div>
             </div>
         </div>
@@ -40,7 +87,7 @@
                     </li>
                     <!--Databoxes-->
                     <li>
-                        <a href="{{ url('/directorio') }}">
+                        <a href="{{ url('/directorio') }">
                             <i class="menu-icon glyphicon glyphicon-tasks"></i>
                             <span class="menu-text"> Directorio </span>
                         </a>
@@ -454,6 +501,9 @@
                 </ul>
                 <!-- /Sidebar Menu -->
             </div>
+
+
+
 
 
 @endsection
